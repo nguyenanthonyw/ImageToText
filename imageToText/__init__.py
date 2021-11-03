3236ffe1cd1fd988f2d868cb4bd7b8ffa60b05b1
Anthony Nguyen <nguyenaanthony@gmail.com>
2021-11-03 18:45:48 -0500

    update

--- imageToText/__init__.py
+++ imageToText/__init__.py
@@ -1,5 +1,9 @@
 import pytesseract as tess
 from PIL import Image
 
-imp = Image.open()
+tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+img = Image.open('CB-Text-Png-34.png')
+text = tess.image_to_string(img)
+
+print(text)
 
